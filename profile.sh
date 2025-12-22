f273060c03d1ab66facd94230dfd9b369318b4a8
Clayton Knittel <cknit1999@gmail.com>
2025-12-21 21:11:35 -0800

    update script

--- profile.sh
+++ profile.sh
@@ -2,6 +2,9 @@
 
 # usage: ./profile.sh <binary> [args...] > onoro.svg
 
-rm perf.data
+set -e
+
+cargo b --profile profiled
+rm -f perf.data
 perf record -g --call-graph dwarf,16384 -- $@ >/dev/null
 perf script -F comm,pid,tid,time,event,ip,sym,dso,trace | stackcollapse-perf.pl | flamegraph.pl > brc.svg
