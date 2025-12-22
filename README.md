a82ff45ee7e60646c8a7ba83d704062042b4bd96
Clayton Knittel <cknit1999@gmail.com>
2025-12-21 20:11:52 -0800

    generate measurements file, readme

---
+++ README.md
@@ -0,0 +1,47 @@
+# Clayton's 1brc
+
+Inspired by https://github.com/gunnarmorling/1brc.
+
+## Rules
+
+* All programming languages allowed.
+* No external libraries that help in nontrivial ways.
+  * Utility libraries are allowed.
+  * If unsure, ask.
+* The rounding of output values must be done using the semantics of IEEE 754 rounding-direction "roundTowardPositive".
+
+## How to structure your program
+
+You should write a program that takes a single command line argument `--input <input_file>`, which is a path to the file you need to parse and print a summary of. For example:
+
+```bash
+./my_program --input ./measurements.txt
+```
+
+Your program should print a summary of the measurements to stdout. It should print the min/mean/max temperature value recorded per weather station in the input file, like this:
+
+```
+{Abha=-23.0/18.0/59.2, Abidjan=-16.2/26.0/67.3, Abéché=-10.0/29.4/69.0, Accra=-10.1/26.4/66.4, Addis Ababa=-23.7/16.0/67.0, Adelaide=-27.8/17.3/58.5, ...}
+```
+
+Note that the weather stations should be sorted alphabetically.
+
+## Input file format
+
+You'll be given a file of newline-separated rows, where each row contains the name of a weather station and a temperature reading at that weather station, separated by a semicolon. The last row of the file will be followed by a newline.
+
+For example, the input file may look like this:
+```
+Abha;-23.0
+Abidjan;29.4
+...
+```
+
+You may assume the following about the format of the input file:
+* The input file is well-formatted and adheres to the following restrictions.
+* The whole input file is valid UTF-8.
+* The name of weather stations is anywhere between 2 - 50 bytes long, and does not contain ';' or '\n' characters.
+  * note: bytes, not characters, which may differ for non-ascii unicode.
+* Temperature readings are between -99.9 and 99.9 (inclusive), always with one fractional digit.
+* There is a maximum of 10,000 unique weather stations.
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,89 @@
-fn main() {
-  println!("Hello, world!");
+use std::{
+  fs::File,
+  io::{BufRead, BufReader, BufWriter, Write},
+  process::ExitCode,
+};
+
+use clap::Parser;
+use rand_distr::Normal;
+
+use crate::error::{BrcError, BrcResult};
+
+use rand::{seq::IteratorRandom, Rng};
+
+mod error;
+
+const WEATHER_STATIONS_PATH: &str = "data/weather_stations.csv";
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+  #[arg(long, default_value_t = 1_000_000_000)]
+  records: u64,
+
+  #[arg(long, default_value_t = 10_000)]
+  unique_cities: u32,
+
+  #[arg(short, long, default_value = "measurements.txt")]
+  output: String,
+}
+
+#[derive(Debug)]
+struct City {
+  name: String,
+  average_temperature: f32,
+}
+
+fn get_weather_stations() -> BrcResult<Vec<City>> {
+  BufReader::new(File::open(WEATHER_STATIONS_PATH)?)
+    .lines()
+    .filter(|line| !line.as_ref().is_ok_and(|line| line.starts_with('#')))
+    .map(|line| {
+      let line = line?;
+      let (name, average_temperature) = line
+        .split_once(';')
+        .ok_or_else(|| BrcError::new(format!("No ';' found in line \"{line}\"")))?;
+      Ok(City {
+        name: name.to_owned(),
+        average_temperature: average_temperature.parse()?,
+      })
+    })
+    .collect()
+}
+
+fn run() -> BrcResult {
+  let mut rng = rand::rng();
+  let args = Args::try_parse()?;
+  let weather_stations = get_weather_stations()?;
+
+  let sampled_stations = weather_stations
+    .iter()
+    .choose_multiple(&mut rng, args.unique_cities as usize);
+
+  let mut output = BufWriter::new(File::create(args.output)?);
+  for _ in 0..args.records {
+    let city = sampled_stations
+      .iter()
+      .choose(&mut rng)
+      .ok_or_else(|| BrcError::new("Unexpected empty weather_stations".to_owned()))?;
+
+    let average_temp = city.average_temperature;
+    let dist = Normal::new(average_temp, 10.)?;
+    let measured_temp = rng.sample(dist).clamp(-99.9, 99.9);
+
+    let line = format!("{};{:.1}\n", city.name, measured_temp);
+    output.write_all(line.as_bytes())?;
+  }
+  output.flush()?;
+
+  Ok(())
+}
+
+fn main() -> ExitCode {
+  if let Err(err) = run() {
+    println!("{err}");
+    ExitCode::FAILURE
+  } else {
+    ExitCode::SUCCESS
+  }
 }
